699528e8f54630811f9c46b85d43247923cccaef
Eduardo Almeida <megaedux@me.com>
2015-03-21 17:25:13 +0000

    First Commit

---
+++ src/pt/up/fe/ia/a4_2/Board.java
@@ -0,0 +1,5 @@
+package pt.up.fe.ia.a4_2;
+
+public class Board {
+
+}
---
+++ src/pt/up/fe/ia/a4_2/Solver.java
@@ -0,0 +1,17 @@
+package pt.up.fe.ia.a4_2;
+
+public class Solver {
+
+    public Solver(Board b) {
+
+    }
+
+    public void solve() {
+
+    }
+
+    private void A_Star() {
+
+    }
+
+}
