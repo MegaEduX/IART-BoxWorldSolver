d5e009648ccbb733ddaca1a9901b613afaf30c90
Eduardo Almeida <megaedux@me.com>
2015-03-30 02:28:17 +0100

    Developed the Board a bit.

--- src/pt/up/fe/ia/a4_2/Board.java
+++ src/pt/up/fe/ia/a4_2/Board.java
@@ -2,4 +2,51 @@ package pt.up.fe.ia.a4_2;
 
 public class Board {
 
+    public enum PieceType {
+        Player,
+        Floor,
+        Wall,
+        Box,
+        IceBox,
+        Exit
+    }
+
+    public enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    private int heuristicG = 0;
+
+    private PieceType[][] boardRepresentation;
+
+    public Board(PieceType[][] boardRep) {
+        boardRepresentation = boardRep;
+    }
+
+    public boolean movePlayer(Direction d) {
+        if (false /* move */) {
+            heuristicG++;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public int getHeuristicG() {
+        return heuristicG;
+    }
+
+    public int getHeuristicH() {
+        //  TODO: Calculate
+
+        return -1;
+    }
+
+    public int getHeuristicF() {
+        return getHeuristicG() + getHeuristicH();
+    }
 }
